7fdaf188ba585cdc9efc54163bfd93ca1f5c4b08
Christian <christ_sapphire@yahoo.com>
2014-11-17 14:53:38 -0500

    before workflow starts

--- client/components/project/project.service.js
+++ client/components/project/project.service.js
@@ -2,19 +2,19 @@
 
 angular.module('hireDotApp')
   .factory('Project', function ($resource) {
-    var Project = $resource('/api/projects')
+    var Project = $resource('/api/projects');
 
     Project.allProjects = [];
 
     Project.search = function(projectName) {
       this.query({ name: projectName }, function(projects) {
-        angular.copy(projects, Project.allProjects)
+        angular.copy(projects, Project.allProjects);
       });
     };
 
     Project.sortBy = function(sortCriteria) {
       this.query(sortCriteria, function(projects) {
-        angular.copy(projects, Project.allProjects)
+        angular.copy(projects, Project.allProjects);
       });
     };
 
