dd956ba611101550afe2a54f3c64405df29acc63
Logle <vqlong2014@gmail.com>
2014-11-20 13:52:11 -0500

    placeholder folder project-page

--- client/app/view/project-page/project-page.controller.js
+++ client/app/view/project-page/project-page.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('hireDotApp')
-  .controller('ProjectPageCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ProjectPageCtrl', function ($scope, Project) {
+  	// Project.search();
+  	$scope.projects = Project.query();
+  	console.log($scope.projects);
+  	// $scope.oneExample = Project.query()[0];
+    // $scope.message = 'Hello';
   });
