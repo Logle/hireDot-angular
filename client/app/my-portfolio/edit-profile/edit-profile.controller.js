06f83890ae297a0000bba2a8cc0cac92cad052ed
rpendse16 <rpendse16@gmail.com>
2014-11-24 12:41:59 -0500

    commit before mergin to master

--- client/app/my-portfolio/edit-profile/edit-profile.controller.js
+++ client/app/my-portfolio/edit-profile/edit-profile.controller.js
@@ -3,4 +3,15 @@
 angular.module('hireDotApp')
   .controller('EditProfileCtrl', function ($scope) {
     $scope.message = 'Hello';
-  });
+	$scope.jobs = [];
+  	$scope.addWork = function () {
+  		var newJob = {
+  			CN: $scope.CN,
+  			title: $scope.title,
+  			sd: $scope.sd,
+  			ed: $scope.ed
+  		}
+  		console.log('adding: ', newJob)
+  		$scope.jobs.push(newJob);
+  };
+});
