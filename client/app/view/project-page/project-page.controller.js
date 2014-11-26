04e846c4bea593a03082635f5e26b61dd70b29e0
Logle <vqlong2014@gmail.com>
2014-11-25 20:53:35 -0500

    refactor project page

--- client/app/view/project-page/project-page.controller.js
+++ client/app/view/project-page/project-page.controller.js
@@ -4,5 +4,13 @@ angular.module('hireDotApp')
   .controller('ProjectPageCtrl', function ($scope, $stateParams, Project) {
     $scope.projectData = Project.get({ id: $stateParams.project_id});
     console.log($scope.projectData);
+  })
+  .directive('developerProfileCard', function(){
+  	return {
+  		restrict: 'E',
+  		scope: {
+  			developer: "=info"
+  		},
+  		templateUrl: 'app/view/project-page/developer-profile-card.html'
+  	};
   });
-  
