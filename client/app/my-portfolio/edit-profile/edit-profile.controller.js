3cb6601f00b305f50032593434c4c85e16f6d59f
Christian Sakai <christianmsakai@gmail.com>
2014-11-28 20:42:06 -0500

    backing up database

--- client/app/my-portfolio/edit-profile/edit-profile.controller.js
+++ client/app/my-portfolio/edit-profile/edit-profile.controller.js
@@ -2,18 +2,17 @@
 
 angular.module('hireDotApp')
   .controller('EditProfileCtrl', function ($scope) {
-    $scope.message = 'Hello';
-	$scope.jobs = [];
-  	$scope.addWork = function () {
-  		$scope.jobs.push($scope.currentWork);
-      $scope.currentWork = {};
-  };
-  $scope.colleges = [];
-    $scope.addSchool = function () {
-      $scope.colleges.push($scope.currentc);
-      $scope.currentc = {};
-  };
-  $scope.remove = function(arrayToRemoveFrom, row) {
-    arrayToRemoveFrom.splice(row,1);
-  };
+	// $scope.jobs = [];
+ //  	$scope.addWork = function () {
+ //  		$scope.jobs.push($scope.currentWork);
+ //      $scope.currentWork = {};
+ //  };
+ //  $scope.colleges = [];
+ //    $scope.addSchool = function () {
+ //      $scope.colleges.push($scope.currentc);
+ //      $scope.currentc = {};
+ //  };
+ //  $scope.remove = function(arrayToRemoveFrom, row) {
+ //    arrayToRemoveFrom.splice(row,1);
+ //  };
 });
