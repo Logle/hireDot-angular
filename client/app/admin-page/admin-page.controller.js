dd1b3c6a430b1543c4b7c5ec110ca6a2c76bf59c
Adam Zerner <azerner3@gmail.com>
2014-12-02 11:54:05 -0500

    got table up with users

--- client/app/admin-page/admin-page.controller.js
+++ client/app/admin-page/admin-page.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('hireDotApp')
-  .controller('AdminPageCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('AdminPageCtrl', function ($scope, $http) {
+  	$http.get('/api/users/getAll')
+  		.success(function(users) {
+  			$scope.users = users;
+  		})
+  		.error(function() {
+  			console.error('unable to get users');
+  		});
   });
